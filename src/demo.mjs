0f74fe11da9b26ea8732d573f25d5a8742e136a6
894risper <zilper894@gmail.com>
2025-06-11 05:12:22 +0300

    updated products route

--- src/demo.mjs
+++ src/demo.mjs
@@ -10,6 +10,14 @@ const mockUsers=[
           {id:3,username:"Joshua", course:"civil"}
 ]
 
+const products= [
+          {id:1,name:"milk"},
+        {id:2,name:"bread"},
+         {id:3,name:"meat"},
+        {id:4,name:"banana"}
+    
+]
+
 app.get("/",(req,res)=>{
     res.send("hello world")
 });
@@ -37,10 +45,34 @@ app.get("/api/users/:id",(req,res)=>{
 
 
 app.get("/api/products",(req,res)=>{
-    res.send([
-        {id:123,name:"milk"},
-        {id:124,name:"bread"}
-    ])
+    res.send(products)
+});
+
+app.get("/api/products/:id",(req,res)=>{
+    console.log(req.params);
+
+    const parsedId= parseInt(req.params.id);
+    if (isNaN(parsedId)) return res.status(400).send({msg :"bad request"});
+
+    console.log(parsedId);
+
+    const findProduct= products.find((product)=>product.id===parsedId)
+    if (!findProduct) return res.sendStatus(404);
+
+    return res.send(findProduct)
+});
+
+app.get('/api/posts',async(req, res)=>{
+    try{
+        const response= await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+        const data = await response.json();
+        res.json(data)
+        
+    }catch(error){
+        res.status(500).json({msg:'failed to fetch comments',error:error.message})
+
+    }
 })
 
 app.listen(PORT,()=>{
